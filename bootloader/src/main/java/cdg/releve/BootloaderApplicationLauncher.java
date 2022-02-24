1aae67e4ba38473f226f0e0166b99262cc16f2a6
IbrahimBakhaled <bakhaledibrahim@gmail.com>
2022-02-24 13:37:45 +0100

    Added Swagger 3 (OpenAPI Documentation) To use Swagger 3 use this link: http://localhost:8080/swagger-ui.html

--- bootloader/src/main/java/cdg/releve/BootloaderApplicationLauncher.java
+++ bootloader/src/main/java/cdg/releve/BootloaderApplicationLauncher.java
@@ -1,10 +1,13 @@
 package cdg.releve;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Releve Bancaire API", version = "2.0", description = "Releve Bancaire Information"))
 public class BootloaderApplicationLauncher {
     public static void main(String[] args) {
         SpringApplication.run(BootloaderApplicationLauncher.class, args);
