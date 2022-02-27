0868db860db633aeaef0c2dd94ca954dc2e441e4
IbrahimBakhaled <bakhaledibrahim@gmail.com>
2022-02-27 21:23:40 +0100

    Dockerized the spring boot application to talk to MongoDB installed on Windows IMPORTANT= Read the docker-compose.yml file line 13/14/15/16 for explanation the connection between spring boot container and mongodb on windows

--- bootloader/src/main/java/cdg/releve/BootloaderApplicationLauncher.java
+++ bootloader/src/main/java/cdg/releve/BootloaderApplicationLauncher.java
@@ -4,8 +4,12 @@ package cdg.releve;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
+//(exclude = {MongoAutoConfiguration.class,MongoDataAutoConfiguration.class})
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Releve Bancaire API", version = "2.0", description = "Releve Bancaire Information"))
 public class BootloaderApplicationLauncher {
--- infrastructure/adapter-persistence-spring-data-mongodb/src/main/java/cdg/releve/persistence/mongodb/config/SpringDataMongodbAdapterConfiguration.java
+++ infrastructure/adapter-persistence-spring-data-mongodb/src/main/java/cdg/releve/persistence/mongodb/config/SpringDataMongodbAdapterConfiguration.java
@@ -4,9 +4,11 @@ package cdg.releve.persistence.mongodb.config;
 import cdg.releve.domain.spi.ReleveBancairePersistencePort;
 import cdg.releve.persistence.mongodb.adapter.ReleveBancaireSpringMongodbAdapter;
 import cdg.releve.persistence.mongodb.repository.ReleveBancaireRepository;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @ComponentScan(basePackages = {"cdg.releve"})
