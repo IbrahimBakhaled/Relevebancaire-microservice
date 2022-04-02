4fb68af962cfbf4e34567644b50f3e88781794c1
bakhaledibrahim <lcdtvseries4 404>
2022-04-02 22:25:20 +0000

    "added docker module"

---
+++ application/adapter-rest/src/main/java/cdg/releve/application/rest/configuration/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package cdg.releve.application.rest.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity // enabling the security on our web app
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+        http.authorizeRequests().antMatchers("/**").permitAll().anyRequest()
+                .authenticated().and().cors().configurationSource(request -> corsConfiguration);
+        http.csrf().disable();
+
+    }
+}
