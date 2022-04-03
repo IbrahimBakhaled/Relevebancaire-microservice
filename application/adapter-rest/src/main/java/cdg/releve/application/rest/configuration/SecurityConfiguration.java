b53d3d7103fbbca1132945f1273301bc5bb33db2
bakhaledibrahim <lcdtvseries4 404>
2022-04-03 16:10:45 +0000

    "deleted controller test"

--- application/adapter-rest/src/main/java/cdg/releve/application/rest/configuration/SecurityConfiguration.java
+++ application/adapter-rest/src/main/java/cdg/releve/application/rest/configuration/SecurityConfiguration.java
@@ -17,6 +17,7 @@ import java.util.List;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
