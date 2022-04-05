bfaae5293dad915336fadfebdcef1cd8cd5333bd
bakhaledibrahim <lcdtvseries4 404>
2022-04-05 05:16:46 +0000

    " fixed controller test and moved application main class"

--- application/adapter-rest/src/test/java/cdg/releve/application/rest/controller/ReleveBancaireControllerImplTest.java
+++ application/adapter-rest/src/test/java/cdg/releve/application/rest/controller/ReleveBancaireControllerImplTest.java
@@ -31,7 +31,6 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
